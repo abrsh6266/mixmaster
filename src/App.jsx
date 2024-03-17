822cb290c52eba345dff5753e87ff901c27e6c57
Abrham <abrsh6266@gmail.com>
2024-03-17 06:55:33 -0800

    creating react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Home page</h2>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h2>About page</h2>
+      </div>
+    ),
+  },
+]);
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
 export default App;
