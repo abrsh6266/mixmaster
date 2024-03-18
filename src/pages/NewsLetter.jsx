fe4d37938ad75d075e2e783855a10f3e4247ce48
Abrham <abrsh6266@gmail.com>
2024-03-18 04:38:12 -0800

    adding error handling

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -6,10 +6,15 @@ const newsletterUrl = "https://www.course-api.com/cocktails-newsletter";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const response = await axios.post(newsletterUrl, data);
-  console.log(response.data);
-  toast.success(response.data.msg);
-  return redirect("/");
+  try {
+    const response = await axios.post(newsletterUrl, data);
+    console.log(response.data);
+    toast.success(response.data.msg);
+    return redirect("/");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
 };
 const Newsletter = () => {
   return (
@@ -27,7 +32,6 @@ const Newsletter = () => {
           className="form-input"
           name="name"
           id="name"
-          required
         />
       </div>
       {/* last name */}
@@ -52,7 +56,6 @@ const Newsletter = () => {
           className="form-input"
           name="email"
           id="email"
-          required
         />
       </div>
       <button
